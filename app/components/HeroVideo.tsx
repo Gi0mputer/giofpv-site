033d7c1e313b746a33e1abed7353ed925f61e454
Giovanni Fantoni <gfantoni@terranovasoftware.eu>
2025-11-22 14:51:11 +0100

    Fix video proportions (85svh) and remove scroll indicator

--- app/components/HeroVideo.tsx
+++ app/components/HeroVideo.tsx
@@ -4,9 +4,9 @@ import { FaChevronDown } from "react-icons/fa";
 
 export function HeroVideo() {
     return (
-        <div className="relative h-[100svh] lg:h-[70svh] w-full lg:max-w-[1400px] lg:mx-auto overflow-hidden flex flex-col lg:block bg-black">
+        <div className="relative h-[100svh] lg:h-[85svh] w-full lg:max-w-[1400px] lg:mx-auto overflow-hidden flex flex-col lg:block bg-black">
             {/* Video Background / Top Section on Mobile */}
-            <div className="relative h-[75%] lg:h-full w-full overflow-hidden group">
+            <div className="relative h-[75%] lg:h-full w-full overflow-hidden">
                 <div className="absolute inset-0 z-0">
                     <iframe
                         src="https://www.youtube.com/embed/kddVKHFSUAw?autoplay=1&mute=1&controls=1&loop=1&playlist=kddVKHFSUAw&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&playsinline=1&enablejsapi=1&vq=hd1080&disablekb=0"
@@ -18,23 +18,6 @@ export function HeroVideo() {
                     <div className="absolute inset-0 bg-black/10 pointer-events-none" />
                     {/* Gradient overlay at bottom to blend with content */}
                     <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-neutral-950 to-transparent pointer-events-none" />
-                </div>
-
-                {/* Desktop Scroll Indicator (Bubble Design) */}
-                <div className="hidden lg:block absolute bottom-4 left-1/2 -translate-x-1/2 pointer-events-none z-20">
-                    {/* Hover detection area - 150px radius (~4cm) */}
-                    <div className="relative w-[150px] h-[150px] pointer-events-auto group">
-                        {/* Clickable bubble - centered */}
-                        <div
-                            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[60px] h-[60px] cursor-pointer"
-                            onClick={() => document.getElementById('gallery')?.scrollIntoView({ behavior: 'smooth' })}
-                        >
-                            {/* Transparent bubble that becomes visible on hover */}
-                            <div className="absolute inset-0 flex items-center justify-center rounded-full border-2 border-white/20 bg-white/5 opacity-0 group-hover:opacity-100 group-hover:border-white/40 group-hover:bg-white/10 transition-all duration-300">
-                                <FaChevronDown className="text-white h-5 w-5 animate-bounce" />
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
 
